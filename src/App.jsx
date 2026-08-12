7f69f223b3c32e65cb2e9cdc59f98ac5a65f0c8f
Nicolas Fasanella <nicolasfasanella@yahoo.com.ar>
2026-08-12 10:37:22 -0300

    Corregir sugerencias e implementar greeting mediante props en ItemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <ItemListContainer />
+      <ItemListContainer greeting="¡Bienvenido a NEOTECH!"/>
       <main>
 
       </main>
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,10 +1,10 @@
 
 import "./ItemListContainer.css";
 
-function ItemListContainer() {
+function ItemListContainer({greeting}) {
   return (
     <section className="item-list-container">
-      <h2>Bienvenido a NEOTECH</h2>
+      <h2>{greeting}</h2>
       <p>Descubrí la mejor tecnología para potenciar tu día a día.</p>
       <button>Ver productos</button>
     </section>
