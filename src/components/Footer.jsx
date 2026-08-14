42c0104c4626ae063f354c7d2d95f101f1b44fc8
Nicolas Fasanella <nicolasfasanella@yahoo.com.ar>
2026-08-14 16:31:36 -0300

    feat: agregar redes sociales al footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,40 @@
 
 import "./Footer.css";
+import { FaLinkedin, FaGithub, FaGlobe } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className="footer">
       <p>© 2026 NEOTECH — Tecnología para el futuro</p>
+
+      <div className="footer-social">
+        <a
+          href="https://www.linkedin.com/in/fasanellanicolas"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+
+        <a
+          href="https://nicolasfasanella.com.ar"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Mi página"
+        >
+          <FaGlobe />
+        </a>
+
+        <a
+          href="https://github.com/DarkNeo-1981"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <FaGithub />
+        </a>
+      </div>
     </footer>
   );
 }
